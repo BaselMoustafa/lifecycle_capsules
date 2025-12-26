5dba0c46d6b6577e1a894b12f2cea32479c697ac
Basel Moustafa <basel.moustafa2000@gmail.com>
2025-12-26 02:39:38 +0200

    export package api

--- lib/lifecycle_capsules.dart
+++ lib/lifecycle_capsules.dart
@@ -1,5 +1,11 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/capsules_state/capsules_state.dart';
+export 'src/capsules_state/adding_capsules_extension.dart';
+
+export 'src/capsules/base/lifecycle_capsule.dart';
+export 'src/capsules/base/operation_capsule.dart';
+export 'src/capsules/base/resource_capsule.dart';
+
+export 'src/capsules/implementations/operations_capsules/add_listener_capsule.dart';
+export 'src/capsules/implementations/resources_capsules/animation_controller_capsule.dart';
+export 'src/capsules/implementations/resources_capsules/text_editing_controller_capsule.dart';
+
